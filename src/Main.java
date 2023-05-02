ae9ebf37237936a807d35b64663653fc6ee8e05c
ambrishba-sacumen <ambrish.ba@sacumen.com>
2023-05-02 15:02:48 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        ArrayList ai=new ArrayList();
+        ai.add("one ");
+        ai.add("two");
+        ai.add("three ");
+
+for(Object o:ai)
+{
+    System.out.println(o);
+}
+    }
+}
